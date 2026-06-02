d808bc27cf3bf22f550850bb85539419a26bbd81
csenando <csenando@github.com>
2026-06-02 12:45:17 -0500

    Fix ENETUNREACH in Nodemailer

--- ticket-backend/src/infrastructure/services/EmailService.ts
+++ ticket-backend/src/infrastructure/services/EmailService.ts
@@ -5,11 +5,13 @@ export class EmailService {
 
     constructor() {
         this.transporter = nodemailer.createTransport({
-            service: 'gmail',
+            host: 'smtp.gmail.com',
+            port: 587,
+            secure: false, // true for 465, false for other ports
             auth: {
                 user: process.env.SMTP_USER,
                 pass: process.env.SMTP_PASS,
-            },
+            }
         });
     }
 
